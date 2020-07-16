5d36a8762a05f391cab0155a257791123ef0c4be
Tudor Alexa <tudor.alexa99@gmail.com>
2020-07-16 17:38:12 +0300

    fix resizing problem

--- CoffeeShop/script.js
+++ CoffeeShop/script.js
@@ -2,6 +2,7 @@
 
 window.onload = function() {
     resizePlates();
+    populateDishes();
 };
 
 window.onresize = function () {
@@ -14,32 +15,49 @@ window.onresize = function () {
 
 function resizePlates() { // function thar resizes the container of all-dishes dynamically
     //get the size of all-dishes container
-    var container = document.getElementById("all-dishes-container");
+    const container = document.getElementById("all-dishes-container");
     //get the size of a box containing information about a dish
-    let card = document.querySelector('.dish-container');
-    let containerSize = container.offsetWidth;
-    let cardSize = card.offsetWidth;
 
-    let fourCards = cardSize * 4;
-    let threeCards = cardSize * 3;
-
-    // console.log(threeCards);
-    // console.log(containerSize);
-    // return;
-
-    if (containerSize < fourCards && containerSize >= threeCards){
-        container.style.width = threeCards + "px";
-        console.log("Resized to 3");
-    }
-    if(containerSize >= fourCards){
-        container.style.width = fourCards + "px";
+    const card = document.querySelector('.dish-container');
+    const containerSize = window.innerWidth;
+    const cardSize = card.offsetWidth;
+    const fourCards = cardSize * 4;
+    const threeCards = cardSize * 3;
+    if (containerSize >= fourCards){
+        container.style.width = `${fourCards}px`;
         console.log("Resized to 4");
     }
-    // console.log("Resize");
-    // console.log(container.offsetWidth);
+
+    else if (containerSize >= threeCards ){
+        container.style.width = `${threeCards}px`;
+        console.log("Resized to 3");
+    }
+
+    else{
+        container.style.width = `${fourCards / 2}px`;
+    }
 }
 
 //
 // function scrollDelay() {
 //     console.log("Scroll");
 // }
+
+function Dish(url, name, category, price){
+    this.url = url;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+}
+
+function dishContainerHTML(dish){
+    const text = '<div class=\"dish-container\"><img class=\"dish\" src=\"' +dish.url + "\"  alt=\"dish\">\n" +
+        "      <p class=\"dish-name\">' + dish.name + ' </p>\n" +
+        "      <p class=\"dish-category\">' + dish</p>\n" +
+        "      <p class=\"dish-price\">$12.00</p>\n" +
+        "    </div>"
+}
+
+function populateDishes() {
+
+}
